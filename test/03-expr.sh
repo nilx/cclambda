cae7584931c7b0dcb0e0b5f9d42b7ca2f10141ce
Nicolas Limare <nicolas.limare@cmla.ens-cachan.fr>
2011-10-09 13:57:00 +0200

    simplify the execution test

--- test/03-expr.sh
+++ test/03-expr.sh
@@ -2,15 +2,17 @@
 #
 # Check the expression support
 
+IN=data/lena.png
+OUT=/dev/null
+
 _test_expr() {
-    IN=data/lena.png
-    OUT=/dev/null
+    EXPR=$1
     ./cclambda $IN "$EXPR" > $OUT
+    CC=gcc ./cclambda $IN "$EXPR" > $OUT
+    CC=gcc CFLAGS=-O3 ./cclambda $IN "$EXPR" > $OUT
 }
 
 _test_canny() {
-    IN=data/lena.png
-    OUT=/dev/null
     (./cclambda $IN "hypot(A_(1,0) - A, A_(0,1) - A)"; \
      ./cclambda $IN "atan2(A_(0,1) - A, A_(1,0) - A) / (2 * M_PI) + .5" ) \
      | ./cclambda - - \
@@ -32,32 +34,16 @@ _log_init
 
 echo "* check expressions"
 _log make -B
-for EXPR in \
-    "(A > .5 ? 1 : 0)" \
-    "(A / 2) + exp(-40. * R2)" \
-    "A_(1,0) - A" \
-#    "hypot(2 * A_(1,0) + A_(1,1) + A_(1,-1) \
-#     - 2 * A_(-1,0) + A_(-1,1) + A_(-1,-1), \
-#     2 * A_(0,1) + A_(1,1) + A_(-1,1) \
-#     - 2 * A_(0,-1) + A_(1,-1) + A_(-1,-1))" \
-    do
-    unset CC
-    unset CFLAGS
-    _log _test_expr "$EXPR"
-    export CC=gcc
-    _log _test_expr "$EXPR"
-    export CFLAGS=-O3
-    _log _test_expr "$EXPR"
-done
-unset CC
-unset CFLAGS
+
+_log _test_expr "(A > .5 ? 1 : 0)"
+_log _test_expr "(A / 2) + exp(-40. * R2)"
+_log _test_expr "A_(1,0) - A"
 _log _test_canny
 export CC=gcc
 _log _test_canny
 export CFLAGS=-O3
 _log _test_canny
-unset CC
-unset CFLAGS
+unset CC CFLAGS
 
 _log make distclean
 
